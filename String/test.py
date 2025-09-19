fcd54dab9fb4c594f9893f477d1cb8bb81b45c81
RichardHtunn <shinthanthtun00@gmail.com>
2025-09-19 10:32:45 +0700

    Format specifier

--- String/test.py
+++ String/test.py
@@ -11,3 +11,17 @@
 # print("4, \U0001F600")
 # print("5, \101")
 # print("6, \141")
+
+# for width in [8,11]:
+#     for precision in [2,3,4,5]:
+#         print(f'{3.14159:{width}.{precision}}')
+
+n = [3.1514, -42, 1024.0]
+for num in n:
+    print(f'{num:>+9,.2f}')
+
+for num in n:
+    print(f'{num:>-9,.2f}')
+
+for num in n :
+    print(f'{num:=+9,.2f}')
