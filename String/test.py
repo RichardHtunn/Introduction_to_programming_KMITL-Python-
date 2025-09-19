a7cf5328fb364dd49df371634d336fd9a4f0b0e4
RichardHtunn <shinthanthtun00@gmail.com>
2025-09-19 10:14:08 +0700

    Unicode Examples

--- String/test.py
+++ String/test.py
@@ -1,1 +1,13 @@
-print('Hello\nWorld')
+# print("Hello\nWorld")
+# print("Name\tAge\tCountry")
+# print("Alice\t25\tUSA")
+# print("Bob\t30\tCanada")
+
+# print("\x48\x65\x6c\x6c\x6f")
+
+# print("1, \N{GREEK CAPITAL LETTER DELTA}")
+# print("2, \N{BLACK HEART SUIT}")
+# print("3, \u03A9")
+# print("4, \U0001F600")
+# print("5, \101")
+# print("6, \141")
